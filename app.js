5b9edb59721781d3646ced9a4708b054f2f58e49
Oh Junseok <y21zzp@gmail.com>
2023-07-13 01:25:22 +0900

    JWT 기본 검증 코드 구현

--- app.js
+++ app.js
@@ -1,51 +1,21 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const app = express();
+const jwt = require("jsonwebtoken");
 
-app.use(cookieParser());
+const payloadData = {
+  myPayloadData: 1234,
+};
 
-app.get("/set-cookie", (req, res) => {
-  let expires = new Date();
-  expires.setMinutes(expires.getMinutes() + 60); // 만료 시간을 60분으로 설정합니다.
+// jwt 생성
+const token = jwt.sign(payloadData, "mysecretKey");
+console.log(token);
 
-  res.cookie("name", "sparta", {
-    expires: expires,
-  });
-  return res.status(200).end();
-});
+// jwt의 payload 데이터를 복호화
+const decodedValue = jwt.decode(token);
+console.log("복호화한 token 입니다.", decodedValue);
 
-app.get("/get-cookie", (req, res) => {
-  // const cookie = req.headers.cookie;
-  const cookies = req.cookies; // cookieparser 미들웨어를 적용했기 때문에 사용할 수 있다.
-  console.log(cookies); // name=sparta
-  return res.status(200).json({ cookies });
-});
+// jwt를 만들었을 때, 사용한 비밀키가 일치하는지 검증
+const decodedValueByVerify = jwt.verify(token, "mysecretKey");
+console.log("decodedValueByVerify : ", decodedValueByVerify);
 
-// 사용자 정보를 저장할 자물쇠(데이터를 저장하는 부분) 세션 파트
-let session = {}; // key - value
-app.get("/set-session", function (req, res, next) {
-  const name = "sparta"; // 세션에 저장 데이터
-  const uniqueInt = Date.now(); // 클라이언트에게 할당할 열쇠
-  session[uniqueInt] = { name }; // 세션에 데이터 저장
-
-  res.cookie("sessionKey", uniqueInt);
-  return res.status(200).end();
-});
-
-app.get("/get-session", (req, res) => {
-  const { sessionKey } = req.cookies;
-  const sessionItem = session[sessionKey];
-
-  console.log(sessionItem);
-  return res.status(200).json({ sessionItem });
-});
-
-// app.get("/get-session", function (req, res, next) {
-//   const { sessionKey } = req.cookies;
-//   const name = session[sessionKey];
-//   return res.status(200).json({ name });
-// });
-
-app.listen(5002, () => {
-  console.log(5002, "포트를 실행했습니다.");
-});
+// jwt를 만들었을 때, 사용한 비밀키가 일치하는지 검증 하지만 에러 발생
+const decodedValueByVerifyToError = jwt.verify(token, "비밀번호 몰루.");
+console.log("decodedValueByVerifyToError : ", decodedValueByVerifyToError);
